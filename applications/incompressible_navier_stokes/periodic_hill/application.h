83f93f12fb7290495d700246a7c2e17bd05707d9
Richard Schussnig <richard.schussnig@gmx.at>
2025-01-14 13:22:51 +0100

    LinePlotStatisticsHomogeneous::do_evaluate_velocity; update_ghost_values(), this time int eh right place.

--- applications/incompressible_navier_stokes/periodic_hill/application.h
+++ applications/incompressible_navier_stokes/periodic_hill/application.h
@@ -447,6 +447,8 @@ private:
     pp_data.output_data.write_q_criterion         = true;
     pp_data.output_data.degree                    = this->param.degree_u;
     pp_data.output_data.write_higher_order        = true;
+    pp_data.output_data.write_aspect_ratio        = true;
+    pp_data.output_data.write_processor_id        = true;
 
     MyPostProcessorData<dim> my_pp_data;
     my_pp_data.pp_data = pp_data;
