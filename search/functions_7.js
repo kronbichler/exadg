e1b86062833b1ced3506059daf333833f8b95aa3
kronbichler <kronbichler@users.noreply.github.com>
2025-12-01 10:42:07 +0000

    deploy: 2c7f73e3f3233d815ef4b9c08e9015d87327ef5c

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['mappingdofvector_0',['MappingDoFVector',['../classExaDG_1_1MappingDoFVector.html#a02610a355efa9e0350be9647356e570b',1,'ExaDG::MappingDoFVector']]],
-  ['matrixfreedata_1',['MatrixFreeData',['../structExaDG_1_1MatrixFreeData.html#a31ec14ac4105a57596590fbac09046e7',1,'ExaDG::MatrixFreeData']]]
+  ['limit_5fcoarsening_5fand_5frefinement_0',['limit_coarsening_and_refinement',['../namespaceExaDG.html#a69104f887bd3aac5e028e98c45b1935b',1,'ExaDG']]],
+  ['load_5fvectors_1',['load_vectors',['../namespaceExaDG.html#aca69f1a8971e73e273eed50e05557ab3',1,'ExaDG::load_vectors(std::vector&lt; std::vector&lt; VectorType * &gt; &gt; &amp;vectors_per_dof_handler, std::vector&lt; dealii::DoFHandler&lt; dim, dim &gt; const * &gt; const &amp;dof_handlers)'],['../namespaceExaDG.html#a40be486066fffd6ff8837076802b63ee',1,'ExaDG::load_vectors(std::vector&lt; std::vector&lt; VectorType * &gt; &gt; &amp;vectors_per_dof_handler, std::vector&lt; dealii::DoFHandler&lt; dim, dim &gt; const * &gt; const &amp;dof_handlers, dealii::DoFHandler&lt; dim &gt; const *dof_handler_mapping, unsigned int const mapping_degree)']]]
 ];
--- search/functions_a.js
+++ search/functions_a.js
@@ -1,20 +1,8 @@
 var searchData=
 [
-  ['save_5fcurrent_5fstate_5fif_5frequired_0',['save_current_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a7a4afd77dd457397b831862cce372cde',1,'ExaDG::preCICE::Adapter']]],
-  ['serialize_1',['serialize',['../classdealspectrum_1_1Interpolator.html#a8283a193758ed7bc6fd68fd2f3cd9847',1,'dealspectrum::Interpolator::serialize(char const *filename)'],['../classdealspectrum_1_1Interpolator.html#a6b09f98b6b45095290fa3ea8269f0c16',1,'dealspectrum::Interpolator::serialize(char const *filename, double const *&amp;src)'],['../classdealspectrum_1_1SpectralAnalysis.html#a5823986be9b1e140e8a408cdcdd5b59c',1,'dealspectrum::SpectralAnalysis::serialize()']]],
-  ['serialized_5fstring_2',['serialized_string',['../namespaceExaDG_1_1Utilities.html#a37f982dd84fb544d186dde3dda54073b',1,'ExaDG::Utilities']]],
-  ['set_5fcoefficient_5fcell_3',['set_coefficient_cell',['../classExaDG_1_1VariableCoefficients.html#aac75b341696a34a88505a352269f5ed6',1,'ExaDG::VariableCoefficients']]],
-  ['set_5fcoefficient_5fface_4',['set_coefficient_face',['../classExaDG_1_1VariableCoefficients.html#ab5f2f622fa96ed41c6ffecd248ef0ac3',1,'ExaDG::VariableCoefficients']]],
-  ['set_5fcoefficient_5fface_5fcell_5fbased_5',['set_coefficient_face_cell_based',['../classExaDG_1_1VariableCoefficients.html#ae5b82fe6876340c749177f1558fc70ee',1,'ExaDG::VariableCoefficients']]],
-  ['set_5fcoefficient_5fface_5fneighbor_6',['set_coefficient_face_neighbor',['../classExaDG_1_1VariableCoefficients.html#aef6d49252b3b89cc167f576cd2171019',1,'ExaDG::VariableCoefficients']]],
-  ['set_5fcoefficients_7',['set_coefficients',['../classExaDG_1_1VariableCoefficients.html#ac2463a7113784b16e28c72fa8a091e4d',1,'ExaDG::VariableCoefficients']]],
-  ['set_5fsolution_5flinearization_8',['set_solution_linearization',['../classExaDG_1_1Structure_1_1NonLinearOperator.html#af8e6f4b4fd6722418388938525b9743c',1,'ExaDG::Structure::NonLinearOperator']]],
-  ['set_5fviscosity_9',['set_viscosity',['../classExaDG_1_1IncNS_1_1GeneralizedNewtonianModel.html#aa97bcec81031e00e48d670b25bb05d7d',1,'ExaDG::IncNS::GeneralizedNewtonianModel::set_viscosity()'],['../classExaDG_1_1IncNS_1_1ViscosityModelBase.html#a6de08076571b34dcf105b8d44a43a0d4',1,'ExaDG::IncNS::ViscosityModelBase::set_viscosity()'],['../classExaDG_1_1IncNS_1_1TurbulenceModel.html#ab375dbb3b31526efa1250df78c3ab7d2',1,'ExaDG::IncNS::TurbulenceModel::set_viscosity()']]],
-  ['set_5fwrite_5fdata_5ftype_10',['set_write_data_type',['../classExaDG_1_1preCICE_1_1CouplingBase.html#aae21d6fc764dbd5051ec7c27cc38d11a',1,'ExaDG::preCICE::CouplingBase']]],
-  ['setup_11',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
-  ['setup_12',['setup',['../classExaDG_1_1InterfaceCoupling.html#a7aa3cfb167de7d9384a7686e7e1f964e',1,'ExaDG::InterfaceCoupling']]],
-  ['spectralanalysis_13',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
-  ['start_14',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
-  ['stop_15',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]],
-  ['string_5fto_5fenum_16',['string_to_enum',['../namespaceExaDG_1_1Utilities.html#a19aec684f8b02f55a5fc67b5f3484f8a',1,'ExaDG::Utilities']]]
+  ['read_5fdeserialization_5fparameters_0',['read_deserialization_parameters',['../namespaceExaDG.html#a08697c7b1c2681f952c75267328f2b0e',1,'ExaDG']]],
+  ['readheader_1',['readHeader',['../classdealspectrum_1_1Setup.html#a398598d40bbb1da95cde226559de2fe9',1,'dealspectrum::Setup']]],
+  ['refine_5flocal_2',['refine_local',['../namespaceExaDG.html#ac26ac75e95c9fbc45eaa00fa0c05a2b7',1,'ExaDG']]],
+  ['reinit_3',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations::reinit()'],['../classExaDG_1_1SolutionField.html#a11a769db2ab15d1be7a96acc1a2cd23b',1,'ExaDG::SolutionField::reinit()']]],
+  ['reload_5fold_5fstate_5fif_5frequired_4',['reload_old_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a0ce4e88f107c66747490f6747705f4d7',1,'ExaDG::preCICE::Adapter']]]
 ];
